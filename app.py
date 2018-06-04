247e521402236f2cf6b3fdf60b1328ceb6dd8f04
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-06-04 11:49:53 +0000

    My hands are typing words

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '5df994dcd2eb9780067bf035589c62ed'
-__version__ = 'ba74dace9678ac3632f4c076f1cc3b3c'
+__vcs_id__ = '1d60b0b2521aa48dc7344616492ec38a'
+__version__ = 'e3c25f559c427590c92f45e347e0a201'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
