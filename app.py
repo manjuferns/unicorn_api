d78bbb38f49ec399846cb8e72bd8917f93289fbd
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-06-05 07:15:30 +0000

    Erratic subject/verb replacements

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '377d93044a42c56a0594d3ae262eaeba'
-__version__ = 'fcebf493627bd4ebbf40943452036e05'
+__vcs_id__ = '080e33234b6eba66d864c90e3f2b50b5'
+__version__ = 'a847ab75642c1a0ef209a6e53bc2e3cd'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
